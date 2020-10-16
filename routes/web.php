b3aaf36e4436c81a65797bac0d21c7d5e07ef33b
Aoyama Momoka <Momoka@MomoMac.local>
2020-10-16 17:09:07 +0900

    change3

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::resource('/todo', 'App\Http\Controllers\TodoController');
 Route::put('/todos/{todo}/complete', 'App\Http\Controllers\TodoController@complete')->name('todo.complete');
 Route::delete('/todos/{todo}/incomplete', 'App\Http\Controllers\TodoController@incomplete')->name('todo.incomplete');
 
 
-//Route::get('/', 'App\Http\Controllers\TodoController@index');
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
