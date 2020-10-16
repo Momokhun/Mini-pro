a4eec090759fde85435157e7a9625c578756936e
Aoyama Momoka <Momoka@MomoMac.local>
2020-10-16 16:27:38 +0900

    change web.php

--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,9 @@ Route::put('/todos/{todo}/complete', 'App\Http\Controllers\TodoController@comple
 Route::delete('/todos/{todo}/incomplete', 'App\Http\Controllers\TodoController@incomplete')->name('todo.incomplete');
 
 
+Route::get('/', 'App\Http\Controllers\TodoController@index');
 
-
-
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 
