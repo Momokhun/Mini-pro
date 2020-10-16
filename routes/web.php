7691c01a65ab26b20d46e852dc00176924ffabef
Aoyama Momoka <Momoka@MomoMac.local>
2020-10-16 16:07:28 +0900

    fix web.php

--- routes/web.php
+++ routes/web.php
@@ -13,25 +13,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-// Route::middleware('auth')->group(function () {
+
 Route::resource('/todo', 'App\Http\Controllers\TodoController');
 Route::put('/todos/{todo}/complete', 'App\Http\Controllers\TodoController@complete')->name('todo.complete');
 Route::delete('/todos/{todo}/incomplete', 'App\Http\Controllers\TodoController@incomplete')->name('todo.incomplete');
-// });
-
-
-// Route::get('/todos', 'App\Http\Controllers\TodoController@index')->name('todo.index');
-
-// Route::get('/todos/create', 'App\Http\Controllers\TodoController@create');
-
-// Route::post('/todos/create', 'App\Http\Controllers\TodoController@store');
-
-// Route::get('/todos/{todo}/edit', 'App\Http\Controllers\TodoController@edit');
-
-// Route::patch('/todos/{todo}/update', 'App\Http\Controllers\TodoController@update')->name('todo.update');
-
-// Route::delete('/todos/{todo}/delete', 'App\Http\Controllers\TodoController@delete')->name('todo.delete');
-
 
 
 
@@ -43,8 +28,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 
