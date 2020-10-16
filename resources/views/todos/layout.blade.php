cd2feadbcbb964f69f913ec69dd3d8e74112faf1
Aoyama Momoka <Momoka@MomoMac.local>
2020-10-16 17:27:22 +0900

    change5

--- resources/views/todos/layout.blade.php
+++ resources/views/todos/layout.blade.php
@@ -5,15 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todoリスト</title>
     <!-- css/app.cssを先に-->
+    <link href="{{ secure_asset('resources/css/app.css') }}" rel="stylesheet">
+    <script src="{{ secure_asset('resources/js/app.js') }}" defer></script>
 
-    @if(app('env') == 'production')
-        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
-        <script src="{{ secure_asset('js/app.js') }}" defer></script>
-    @else
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    @endif
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/style.css') }}"> --}}
     <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a463021397.js" crossorigin="anonymous"></script>
     @livewireStyles
